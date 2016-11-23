3360692181ed93c309e4542d4f60d3658d52be3e
Arturo Peroni <arperoni@gmail.com>
2016-11-23 20:03:59 -0300

    "totals" module for processing CSV file.

---
+++ totals.py
@@ -0,0 +1,26 @@
+import csv
+from collections import defaultdict
+
+# Column indexes
+TOTAL = 1
+ADVERTISER = 2
+CAMPAIGN = 3
+MEDIUM = 4
+
+def compute_from(file):
+    advertisers = defaultdict(lambda: 0)
+    campaigns = defaultdict(lambda: 0)
+    mediums = defaultdict(lambda: 0)
+    total = 0
+
+    with open(file, newline='') as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader) # Skips headers row.
+        for row in reader:
+            amount = float(row[TOTAL])
+            advertisers[row[ADVERTISER]] += amount
+            campaigns[row[CAMPAIGN]] += amount
+            mediums[row[MEDIUM]] += amount
+            total += amount
+    
+    return advertisers, campaigns, mediums, total
