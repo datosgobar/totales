7169264c14d152347ad9b32d55bd43e9be644e97
Arturo Peroni <arperoni@gmail.com>
2016-11-23 22:24:38 -0300

    Listing totals by groups: first iteration.

--- application.py
+++ application.py
@@ -5,12 +5,17 @@ import totals
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def index():    
+    return render_template('index.html', totals=get_totals())
 
-@app.route('/totales')
+
 def get_totals():
     results = totals.compute_from('pauta-oficial-primer-semestre-2016.csv')
     advertisers, campaigns, media, total = results
-    
-    return 'This should return totals computed from a CVS file.'
+
+    return {
+        'advertisers': advertisers,
+        'campaigns': campaigns,
+        'media': media,
+        'total': total
+    }
